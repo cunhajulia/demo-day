b15fbc9931675b5b4ebf7fb728e1d45367111962
Julia Cunha <cunha.t.julia@gmail.com>
2023-05-22 14:13:50 -0400

    cssresetetc

--- public/main.js
+++ public/main.js
@@ -51,32 +51,7 @@ const submitButton = document.getElementById('clickMe');
 //   { barcode: "SAECK44444", status: "in storage" }
 // ];
 
-// profile timeline ----------------
-// VARIABLES
-const elH = document.querySelectorAll(".timeline li > div");
 
-// START
-window.addEventListener("load", init);
-
-function init() {
-  setEqualHeights(elH);
-}
-
-// SET EQUAL HEIGHTS
-function setEqualHeights(el) {
-  let counter = 0;
-  for (let i = 0; i < el.length; i++) {
-    const singleHeight = el[i].offsetHeight;
-
-    if (counter < singleHeight) {
-      counter = singleHeight;
-    }
-  }
-
-  for (let i = 0; i < el.length; i++) {
-    el[i].style.height = `${counter}px`;
-  }
-}
 // ------------------------------------------------
 
 // profile tabs for contact info, docs and notes
